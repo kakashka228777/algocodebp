968d7f1431e22e41f7765c95470c81a9e1620468
kakashka228777 <135365281+kakashka228777@users.noreply.github.com>
2025-01-07 14:02:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-CHAT_ID = "@CHAT"
-TELEGRAM_TOKEN = "TOKEN"
+CHAT_ID = "@algonotify25bp"
+TELEGRAM_TOKEN = "7617310563:AAG9oUqE4-lpMVIxvFXSsSdwWF0Mm3_IKBQ"
 
-STANDINGS_PAGES = []
+STANDINGS_PAGES = ["https://algocode.ru/standings_data/bp_fall_2024/"]
 
 TIMEZONE = "Europe/Moscow"
-COOLDOWN = 40
+COOLDOWN = 5
 SAVE_FULL_ARCHIVE = False
 ARCHIVE_FORMAT = "archive/%Y/%B/%d"
